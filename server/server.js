d694a2e3034e57fbfaae7bf486721b5bc65da65d
KristienN <62649348+KristienN@users.noreply.github.com>
2021-06-14 18:00:40 +0200

    add database connection

--- server/server.js
+++ server/server.js
@@ -4,12 +4,24 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
 
+
+//.env file
+require('dotenv').config();
+
 app.use(cors());
-app.use(express.json());
+
+
+// Mongoose connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useCreateIndex: true,useUnifiedTopology: true, useNewUrlParser: true});
+const connection = mongoose.connection;
+connection.once("open", (err)=>{
+    if(err) throw err;
+    console.log("Mongo DB connected")
+});
+
 
 app.listen(port, (err)=>{
     if(err) throw err;
     console.log(`LIstening on port: ${port}.`);
 });
-
-// Hello World
